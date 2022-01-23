4a635e65e2211c3a81d2e4c4754f47909ab93c81
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-22 23:07:49 -0600

    Add project files.

---
+++ CountingCandles_Original/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingCandles_Original
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] birthdayCandles = new int[] { 4, 7, 3, 7, 7, 2, 1, 3 };
+            int tallestCandle = birthdayCandles.Max();
+            List<int> tallestCandles = new List<int>();
+
+            for(int i = 0; i < birthdayCandles.Length; i++)
+            {
+                if(birthdayCandles[i] == tallestCandle)
+                {
+                    tallestCandles.Add(birthdayCandles[i]);
+                }
+            }
+            ;
+
+
+            Console.WriteLine("the tallest candles are " + tallestCandle + " unit high");
+            Console.WriteLine("there are " + tallestCandles.Count + " candles with this height");
+            Console.WriteLine("the total  units of measurement for tallest candles is " + tallestCandles.Sum()) ;
+        }
+    }
+}
